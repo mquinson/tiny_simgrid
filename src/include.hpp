e3c084cf0d8778c1994a9c0ff9c5f9b30e65c21e
Ehsan Azimi <eazimi@ehsan.irisa.fr>
2020-07-06 12:53:59 +0200

    TEST_11() to TEST_17 introduced, check them, I guess adding mailbox doesn't work well

--- src/include.hpp
+++ src/include.hpp
@@ -11,6 +11,7 @@
 #include "Api/CheckerSide.hpp"
 
 using TransitionActivity = tiny_simgrid::app::AppSide::TransitionActivity;
+using TransitionType = tiny_simgrid::app::AppSide::TransitionType;
 using AppSide = tiny_simgrid::app::AppSide;
 using CheckerSide = tiny_simgrid::api::CheckerSide;
 static int nbInt = 0;
@@ -215,6 +216,193 @@ void TEST_10() {
     std::vector<unsigned int> config {4, 4, 4};
 
     make_test(app, config, 18);
+}
+
+void TEST_11() {
+    // the first simix model -> 2 traces
+               // Transition (maiboxid, commid, type)
+    auto tr_0 = app->create_transition(1, 1, TransitionType::Isend); // p0
+    auto tr_1 = app->create_transition(1, 1, TransitionType::wait); // p0
+    auto tr_2 = app->create_transition(1, 1, TransitionType::Isend); // p1
+    auto tr_3 = app->create_transition(1, 1, TransitionType::Ireceive); // p2
+
+    // actors
+    auto actor_0 = app->create_actor(0, std::move(*tr_0), std::move(*tr_1));
+    auto actor_1 = app->create_actor(1, std::move(*tr_2));
+    auto actor_2 = app->create_actor(2, std::move(*tr_3));
+    app->add_to_actor_list(std::move(*actor_0), std::move(*actor_1), std::move(*actor_2));
+
+    // mailbox
+    app->add_to_mailbox_list(1);
+
+    // config
+    std::vector<unsigned int> config {4, 3};
+
+    make_test(app, config, 9);
+}
+
+void TEST_12() {
+    // the 2nd simix model -> 1 trace
+    // Transition (maiboxid, commid, type)
+    auto tr_0 = app->create_transition(1, 1, TransitionType::Isend); // p0
+    auto tr_1 = app->create_transition(1, 1, TransitionType::wait); // p0
+    auto tr_2 = app->create_transition(2, 1, TransitionType::Isend); // p1
+    auto tr_3 = app->create_transition(1, 1, TransitionType::Ireceive); // p2
+
+    // actors
+    auto actor_0 = app->create_actor(0, std::move(*tr_0), std::move(*tr_1));
+    auto actor_1 = app->create_actor(1, std::move(*tr_2));
+    auto actor_2 = app->create_actor(2, std::move(*tr_3));
+    app->add_to_actor_list(std::move(*actor_0), std::move(*actor_1), std::move(*actor_2));
+
+    // mailbox
+    app->add_to_mailbox_list(1);
+
+    // config
+    std::vector<unsigned int> config {4};
+
+    make_test(app, config, 4);
+}
+
+void TEST_13() {
+    // the first simix model -> 2 traces
+    // Transition (maiboxid, commid, type)
+    auto tr_0 = app->create_transition(1, 1, TransitionType::Isend); // p0
+    auto tr_1 = app->create_transition(1, 1, TransitionType::wait); // p0
+    auto tr_2 = app->create_transition(1, 1, TransitionType::Isend); // p1
+    auto tr_3 = app->create_transition(1, 1, TransitionType::wait); // p1
+    auto tr_4 = app->create_transition(1, 1, TransitionType::Ireceive); // p2
+    auto tr_5 = app->create_transition(1, 1, TransitionType::wait); // p2
+
+    // actors
+    auto actor_0 = app->create_actor(0, std::move(*tr_0), std::move(*tr_1));
+    auto actor_1 = app->create_actor(1, std::move(*tr_2), std::move(*tr_3));
+    auto actor_2 = app->create_actor(2, std::move(*tr_4), std::move(*tr_5));
+    app->add_to_actor_list(std::move(*actor_0), std::move(*actor_1), std::move(*actor_2));
+
+    // mailbox
+    app->add_to_mailbox_list(1);
+
+    // config
+    std::vector<unsigned int> config {5, 5};
+
+    make_test(app, config, 13);
+}
+
+void TEST_14() {
+    // the first simix model -> 2 traces
+    // Transition (maiboxid, commid, type)
+
+    auto tr_0 = app->create_transition(1, 1, TransitionType::Isend); // p0
+    auto tr_1 = app->create_transition(1, 1, TransitionType::wait); // p0
+    auto tr_2 = app->create_transition(1, 1, TransitionType::Isend); // p1
+    auto tr_3 = app->create_transition(1, 1, TransitionType::wait); // p1
+    auto tr_4 = app->create_transition(1, 1, TransitionType::Ireceive); // p2
+    auto tr_5 = app->create_transition(1, 1, TransitionType::wait); // p2
+    auto tr_6 = app->create_transition(1, 1, TransitionType::Ireceive); // p2
+    auto tr_7 = app->create_transition(1, 1, TransitionType::wait); // p2
+
+    // actors
+    auto actor_0 = app->create_actor(0, std::move(*tr_0), std::move(*tr_1));
+    auto actor_1 = app->create_actor(1, std::move(*tr_2), std::move(*tr_3));
+    auto actor_2 = app->create_actor(2, std::move(*tr_4), std::move(*tr_5), std::move(*tr_6), std::move(*tr_7));
+    app->add_to_actor_list(std::move(*actor_0), std::move(*actor_1), std::move(*actor_2));
+
+    // mailbox
+    app->add_to_mailbox_list(1);
+
+    // config
+    std::vector<unsigned int> config {8, 8};
+
+    make_test(app, config, 22);
+}
+
+void TEST_15() {
+    // the first simix model -> 6 traces
+    // Transition (maiboxid, commid, type)
+    auto tr_0 = app->create_transition(1, 1, TransitionType::Isend); // p0
+    auto tr_1 = app->create_transition(1, 1, TransitionType::wait); // p0
+    auto tr_2 = app->create_transition(1, 1, TransitionType::Isend); // p1
+    auto tr_3 = app->create_transition(1, 1, TransitionType::wait); // p1
+    auto tr_4 = app->create_transition(1, 1, TransitionType::Ireceive); // p2
+    auto tr_5 = app->create_transition(1, 1, TransitionType::wait); // p2
+    auto tr_6 = app->create_transition(1, 2, TransitionType::Ireceive); // p2
+    auto tr_7 = app->create_transition(1, 2, TransitionType::wait); // p2
+    auto tr_8 = app->create_transition(1, 1, TransitionType::Isend); // p3
+    auto tr_9 = app->create_transition(1, 1, TransitionType::wait); // p3
+
+
+    // actors
+    auto actor_0 = app->create_actor(0, std::move(*tr_0), std::move(*tr_1));
+    auto actor_1 = app->create_actor(1, std::move(*tr_2), std::move(*tr_3));
+    auto actor_2 = app->create_actor(2, std::move(*tr_4), std::move(*tr_5), std::move(*tr_6), std::move(*tr_7));
+    auto actor_3 = app->create_actor(3, std::move(*tr_8), std::move(*tr_9));
+    app->add_to_actor_list(std::move(*actor_0), std::move(*actor_1), std::move(*actor_2), std::move(*actor_3));
+
+    // mailbox
+    app->add_to_mailbox_list(1);
+
+    // config
+    std::vector<unsigned int> config {9, 9, 9, 9, 9, 9};
+
+    make_test(app, config, 59);
+}
+
+void TEST_16() {
+    // the first simix model -> 6 traces
+    // Transition (maiboxid, commid, type)
+    auto tr0_0 = app->create_transition(1, 1, TransitionType::Ireceive); // p0
+    auto tr0_1 = app->create_transition(1, 1, TransitionType::wait); // p0
+    auto tr0_2 = app->create_transition(1, 2, TransitionType::Ireceive); // p0
+    auto tr0_3 = app->create_transition(1, 2, TransitionType::wait); // p0
+    auto tr0_4 = app->create_transition(1, 3, TransitionType::Ireceive); // p0
+    auto tr0_5 = app->create_transition(1, 3, TransitionType::wait); // p0
+    auto tr1_0 = app->create_transition(1, 1, TransitionType::Isend); // p1
+    auto tr1_1 = app->create_transition(1, 1, TransitionType::wait); // p1
+    auto tr2_0 = app->create_transition(1, 1, TransitionType::Isend); // p2
+    auto tr2_1 = app->create_transition(1, 1, TransitionType::wait); // p2
+    auto tr3_0 = app->create_transition(1, 1, TransitionType::Isend); // p3
+    auto tr3_1 = app->create_transition(1, 1, TransitionType::wait); // p3
+
+    // actors
+    auto actor_0 = app->create_actor(0, std::move(*tr0_0), std::move(*tr0_1),
+                                     std::move(*tr0_2), std::move(*tr0_3),
+                                     std::move(*tr0_4), std::move(*tr0_5));
+    auto actor_1 = app->create_actor(1, std::move(*tr1_0), std::move(*tr1_1));
+    auto actor_2 = app->create_actor(2, std::move(*tr2_0), std::move(*tr2_1));
+    auto actor_3 = app->create_actor(3, std::move(*tr3_0), std::move(*tr3_1));
+    app->add_to_actor_list(std::move(*actor_0), std::move(*actor_1), std::move(*actor_2), std::move(*actor_3));
+
+    // mailbox
+    app->add_to_mailbox_list(1);
+
+    // config
+    std::vector<unsigned int> config {12, 12, 12, 12, 12, 12};
+
+    make_test(app, config, 90);
+}
+
+void TEST_17() {
+    //  3 traces
+    // Transition (maiboxid, commid, type)
+    auto tr0_0 = app->create_transition(1, 1, TransitionType::Isend); // p0
+    auto tr0_1 = app->create_transition(1, 1, TransitionType::test); // p0
+    auto tr1_0 = app->create_transition(1, 1, TransitionType::Isend); // p1
+    auto tr2_0 = app->create_transition(1, 1, TransitionType::Ireceive); // p2
+
+    // actors
+    auto actor_0 = app->create_actor(0, std::move(*tr0_0), std::move(*tr0_1));
+    auto actor_1 = app->create_actor(1, std::move(*tr1_0));
+    auto actor_2 = app->create_actor(2, std::move(*tr2_0));
+    app->add_to_actor_list(std::move(*actor_0), std::move(*actor_1), std::move(*actor_2));
+
+    // mailbox
+    app->add_to_mailbox_list(1);
+
+    // config
+    std::vector<unsigned int> config {4, 4, 4};
+
+    make_test(app, config, 14);
 }
 
 #endif // INCLUDE_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,23 @@ int main(int argc, char** argv)
     auto begin = clock();
 
     switch (example) {
-    case 1: TEST_1(); break;
-    case 2: TEST_2(); break;
-    case 3: TEST_3(); break;
-    case 4: TEST_4(); break;
-    case 5: TEST_5(); break;
-    case 6: TEST_6(); break;
-    case 7: TEST_7(); break;
-    case 8: TEST_8(); break;
-    case 9: TEST_9(); break;
+    case  1: TEST_1();  break;
+    case  2: TEST_2();  break;
+    case  3: TEST_3();  break;
+    case  4: TEST_4();  break;
+    case  5: TEST_5();  break;
+    case  6: TEST_6();  break;
+    case  7: TEST_7();  break;
+    case  8: TEST_8();  break;
+    case  9: TEST_9();  break;
     case 10: TEST_10(); break;
+    case 11: TEST_11(); break;
+    case 12: TEST_12(); break;
+    case 13: TEST_13(); break;
+    case 14: TEST_14(); break;
+    case 15: TEST_15(); break;
+    case 16: TEST_16(); break;
+    case 17: TEST_17(); break;
     }
 
     auto end = clock();
