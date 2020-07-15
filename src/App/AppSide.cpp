96c0959414317802f849546f8c6b1cdc44272657
Ehsan Azimi <eazimi@ehsan.irisa.fr>
2020-07-15 16:30:53 +0200

    AppSide class introduced, it's currently isolated

---
+++ src/App/AppSide.cpp
@@ -0,0 +1,57 @@
+#include "AppSide.h"
+#include "Transition.h"
+#include "Actor.h"
+#include "Mailbox.h"
+
+namespace app {
+
+const char* ListTypeName[] = {"transition", "actor", "mailbox"};
+const char* TransitionTypeName[] = {"Isend", "Ireceive", "Test", "Wait"};
+
+Transition* AppSide::create_transition(int mailbox_id, int communication_id, TransitionType type) const {
+    return new Transition(mailbox_id, communication_id, TransitionTypeName[static_cast<int>(type)]);
+}
+
+Transition* AppSide::create_transition(TransitionActivity activity, int access_variable) const {
+    return new Transition(static_cast<int>(activity), access_variable);
+}
+
+template<typename... Ts>
+Actor* AppSide::create_actor(int actor_id, Ts&&... ts) {
+    std::vector<Transition> vec_tr;
+    unpack_params(vec_tr, std::forward<Ts>(ts)...);
+    return (new Actor(actor_id, vec_tr));
+}
+
+template<typename... Ts>
+void AppSide::add_to_actor_list(Ts&&... ts) {
+    std::vector<Actor> vec_ac;
+    unpack_params(vec_ac, std::forward<Ts>(ts)...);
+    actors_list_.clear();
+    actors_list_ = std::move(vec_ac);
+}
+
+template<typename... Ts>
+void AppSide::add_to_mailbox_list(Ts... ts) {
+    std::vector<int> vec_mb_id;
+    unpack_params(vec_mb_id, std::forward<Ts>(ts)...);
+    mailbox_list_.clear();
+    for(auto i : vec_mb_id) {
+        auto mb = Mailbox(i);
+        mailbox_list_.push_back(std::move(mb));
+    }
+}
+
+template<typename T, typename... Rest>
+void AppSide::unpack_params(std::vector<T>& vec, T&& t, Rest&&... rest) const {
+    vec.push_back(std::forward<T>(t));
+    unpack_params(vec, std::forward<Rest>(rest)...);
+}
+
+template<typename T>
+void AppSide::unpack_params(std::vector<T>& vec, T&& t) const {
+    vec.push_back(std::forward<T>(t));
+}
+
+
+} // namespace app
---
+++ src/App/AppSide.h
@@ -0,0 +1,64 @@
+#ifndef APPSIDE_H
+#define APPSIDE_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+namespace app {
+
+class Transition;
+class Actor;
+class Mailbox;
+
+class AppSide {
+public:
+    enum class TransitionActivity {
+        read = 0,
+        write,
+        mutex_lock,
+        mutex_unlock
+    };
+
+    enum TransitionType {
+        Isend = 0,
+        Ireceive,
+        test,
+        wait
+    };
+
+    AppSide() = default;
+    AppSide(AppSide const&) = default;
+    AppSide& operator=(AppSide const&) = default;
+    ~AppSide() = default;
+
+    Transition* create_transition(TransitionActivity activity, int access_variable) const;
+    Transition* create_transition(int mailbox_id, int communication_id, TransitionType type) const;
+
+    template<typename... Ts>
+    Actor* create_actor(int actor_id, Ts&&... ts);
+
+    template<typename... Ts>
+    void add_to_actor_list(Ts&&... ts);
+
+    template<typename... Ts>
+    void add_to_mailbox_list(Ts... ts);
+
+    inline std::vector<Actor> get_actor_list() const { return actors_list_; }
+    inline std::vector<Mailbox> get_mailbox_list() const { return mailbox_list_; }
+
+private:
+    std::vector<Actor> actors_list_;
+    std::vector<Mailbox> mailbox_list_;
+
+    template<typename T, typename... Rest>
+    void unpack_params(std::vector<T>& vec, T&& t, Rest&&... rest) const;
+
+    template<typename T>
+    void unpack_params(std::vector<T>& vec, T&& t) const;
+};
+
+} // namespace app
+
+#endif // APPSIDE_H
